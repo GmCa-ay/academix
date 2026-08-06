cc4fae52db352abe01bfa9ef22798230224436d6
GmCa-ay <gmcaay.dev@gmail.com>
2026-08-06 19:22:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          500: '#22c55e',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d'
+        }
+      }
+    }
+  }
+};
